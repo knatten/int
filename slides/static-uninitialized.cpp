d8db720e2e393f0fa41f5a809550725798d36696
Anders Schau Knatten <anders@knatten.org>
2014-05-04 11:47:05 +0200

    Initial import

---
+++ slides/static-uninitialized.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+static int i;
+
+int main()
+{
+   cout << i;
+}
---
+++ slides/static_variables.cpp
@@ -0,0 +1,16 @@
+int a;
+static int b;
+
+class Ohlson
+{
+    int c;
+    static int d;
+};
+
+void x()
+{
+    int e;
+    static int f;
+}
+
+int main(){}
